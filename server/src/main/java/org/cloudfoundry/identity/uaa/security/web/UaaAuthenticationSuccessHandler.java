0685bf222618b49b9c9c8ee0924f2f1a0fdeccdb
rnuriev <rnuriev@dicomresearch.com>
2016-07-14 13:37:51 +0300

    [NF] temp login check.

--- server/src/main/java/org/cloudfoundry/identity/uaa/security/web/UaaAuthenticationSuccessHandler.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/security/web/UaaAuthenticationSuccessHandler.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.WebAttributes;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Collection;
 
 /**
  * @author rnuriev
@@ -24,11 +26,21 @@ public class UaaAuthenticationSuccessHandler implements AuthenticationSuccessHan
     final Logger logger = LoggerFactory.getLogger(getClass());
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
+
+    public static final String TEMP_LOGIN_SCOPE = "hb.login.temp";
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        // todo: temporary impl
-        if (authentication.getName().equals("q1")) {
+        final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        boolean isTempLogin = false;
+        for (GrantedAuthority grantedAuthority : authorities) {
+            final String authority = grantedAuthority.getAuthority();
+            if (authority != null && authority.equals(TEMP_LOGIN_SCOPE)) {
+                isTempLogin = true;
+            }
+        }
+        if (!isTempLogin) {
             successRedirectHandler.onAuthenticationSuccess(request, response, authentication);
         } else {
             handle(request, response, authentication);
